e74e3e1b635831d2b6b61740910343678c9b0755
12101111 <w12101111@gmail.com>
2024-02-21 16:56:40 +0800

    Allow to disable initialize_all

--- src/targets.rs
+++ src/targets.rs
@@ -827,6 +827,7 @@ impl Target {
         }
     }
 
+    #[cfg(not(feature = "disable-alltargets-init"))]
     pub fn initialize_native(config: &InitializationConfig) -> Result<(), String> {
         use llvm_sys::target::{
             LLVM_InitializeNativeAsmParser, LLVM_InitializeNativeAsmPrinter, LLVM_InitializeNativeDisassembler,
@@ -873,6 +874,7 @@ impl Target {
         Ok(())
     }
 
+    #[cfg(not(feature = "disable-alltargets-init"))]
     pub fn initialize_all(config: &InitializationConfig) {
         use llvm_sys::target::{
             LLVM_InitializeAllAsmParsers, LLVM_InitializeAllAsmPrinters, LLVM_InitializeAllDisassemblers,
